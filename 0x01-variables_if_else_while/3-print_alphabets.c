2ceac819849779022b53e82b3d86eba89b8492b6
HABIMANA Nyiringoma Tresor <tresornyiringoma@gmail.com>
2022-07-06 12:23:46 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,22 @@
-#include <stdio.h>                                                  
-                                                                    
-/**                                                                 
- * main ->  prints the alphabet in lowercase, and then in uppercase
- * Return : always 0                                                
- */                                                                 
-int main (void)                                                     
-{                                                                   int ch;
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (Sucess/correct)
+ */
+int main(void)
 
-for (ch=97;ch<=122;ch++)                                    
-				        {                 
-    putchar(ch);                                        
-								   
-								    for (ch=65; ch<=90; ch++) 
-	    {
-							                putchar(ch);
-									        }            
-putchar(10); /*this is an ASCII code for a new line*/                                      
-					
-return (0);                                                 
-}
+{
+	char lower_case;
+	char upper_case;
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+		putchar(lower_case);
+	}
+
+
+	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+	{
+		putchar(upper_case);
+	}
