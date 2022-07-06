f61bc8dfc5ce20108be3c4352cf1187be1c28d5d
HABIMANA Nyiringoma Tresor <tresornyiringoma@gmail.com>
2022-07-06 12:11:14 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Successs/correct)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Successs/correct)
+ */
 
 int main(void)
 {
