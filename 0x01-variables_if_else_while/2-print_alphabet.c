1f529dc8600423b31c99b649934b63c8bab41ac1
HABIMANA Nyiringoma Tresor <tresornyiringoma@gmail.com>
2022-07-06 11:46:18 -0700

    Task 0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main ->  prints the alphabet in lowercase, followed by a new line
+ * REturn : always 0
+ */
+int main (void)
+{
+	int ch;
+	for (ch=97;ch<=122;ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+
+	return (0);
+}
