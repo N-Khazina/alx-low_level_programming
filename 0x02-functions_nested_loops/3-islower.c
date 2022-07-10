8ffbd7964860c70dcd20fcc0598a4c2966f83655
HABIMANA Nyiringoma Tresor <tresornyiringoma@gmail.com>
2022-07-10 08:25:05 -0700

    function that checks for lowercase character.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,18 +5,8 @@
  *  @c: contains value to be compared
  *  Return: Always 0.
  */
+
 int _islower(int c)
-
 {
-
-if (c > 'a' && c < 'z')
-{
-return (1);
-}
-
-else
-{
-return (0);
-}
-
+		return (c >= 'a' && c <= 'z');
 }
