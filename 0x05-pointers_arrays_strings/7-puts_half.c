139d0ac303f791ca8b88195b394307e318a77868
HABIMANA Nyiringoma Tresor <tresornyiringoma@gmail.com>
2022-07-14 01:47:05 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - print second half of a string
+ * @str: char array string type
+ * Description: If odd number of chars, print (length - 1) / 2
+ */
+void puts_half(char *str)
+{
+	int i, last;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	last = (i + 1) / 2;
+
+	for (i = last; str[i]; i++)
+	{
+		_putchar (str[i]);
+	}
+
+	_putchar ('\n');
+}
