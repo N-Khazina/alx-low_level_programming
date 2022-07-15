5b288a9d1b0850d03cc7b6a933d139dda6467ae9
HABIMANA Nyiringoma Tresor <tresornyiringoma@gmail.com>
2022-07-15 02:18:23 -0700

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,42 +5,45 @@
  */
 void print_times_table(int n)
 {
-	int i, j, k;
+	int a = 0, rep, b;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+
+	while (a <= n)
 	{
-		for (j = 0; j <= n; j++)
+		for (b = 0; b <= n; b++)
 		{
-			k = j * i;
-			if (j == 0)
+			rep = a * b;
+			if (b == 0)
 			{
-				_putchar(k + '0');
+				_putchar('0' + rep)
 			}
-			 else if (k < 10 && j != 0)
-			 {
-				 _putchar(',');
-				 _putchar(' ');
-				 _putchar(' ');
-				 _putchar(' ');
-				 _putchar(k + '0');
-			 }
-			else if (k >= 10 && k < 100)
+			else if (rep < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + rep);
+			}
+			else if (rep < 100)
+			{
+				_putchar(' ');
+				_putchar('0' + rep / 10);
+				_putchar('0' + rep % 10);
+			}
+			else
+			{
+				_putchar('0' + rep / 100);
+				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
+			}
+			if (b < n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-			else if (k >= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 100) + '0');
-				_putchar(((k / 10) % 10) + '0');
-				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
+		a++;
 	}
 }
