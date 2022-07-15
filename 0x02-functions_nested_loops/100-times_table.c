d07030bdfced3e730102ef404a3ec8c0a1e0a8b3
HABIMANA Nyiringoma Tresor <tresornyiringoma@gmail.com>
2022-07-15 02:32:17 -0700

    Task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,8 +13,10 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
+			for (j = 0; j <= n; j++)
+			{
 			if ((i * j) < 10)
 			{
 				if (j != 0)
@@ -39,4 +41,5 @@ void print_times_table(int n)
 		}
 		_putchar('\n');
 	}
+}
 }
