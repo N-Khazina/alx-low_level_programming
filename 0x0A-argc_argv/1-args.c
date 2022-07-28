c84302a2e71df8fcf104352a43e3a87097c9c26e
HABIMANA Nyiringoma Tresor <tresornyiringoma@gmail.com>
2022-07-28 11:47:07 -0700

    my commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - prints all arguments it received
- * @argc: argument count
- * @argv: argument vector
- * Return: returns 0
+ * main - prints the number of arguments
+ * @argc: number of arguments
+ * @argv:array of arguments
+ * Return: the number of arguments
  */
-int main(int argc, char *argv[])
-{
-	int i;
 
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
