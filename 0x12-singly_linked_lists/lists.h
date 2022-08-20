350c54d76ba445a1244efbb7cd94867067c6aaac
HABIMANA Nyiringoma Tresor <tresornyiringoma@gmail.com>
2022-08-21 00:17:57 +0200

    my commit

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,9 +16,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
